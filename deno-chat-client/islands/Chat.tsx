04137e92dba925fd5f822edf1eaf18367a303c80
Aalaap Ghag <aalaap@gmail.com>
2022-04-21 16:42:25 +0530

    Remove unused import

---
+++ deno-chat-client/islands/Chat.tsx
@@ -0,0 +1,36 @@
+/** @jsx h */
+import {
+  h,
+  useCallback,
+  useEffect,
+  useState,
+} from "../client_deps.ts";
+
+interface Message {
+  text: string;
+}
+
+export default function Chat() {
+  const [messages, setMessages] = useState<Message[]>([]);
+  const API_BASE_URL = Deno.env.get("API_BASE_URL");
+
+  const getMessages = useCallback(async () => {
+    const res = await fetch(`${API_BASE_URL}/messages`);
+    const data = await res.json();
+    setMessages(data);
+  });
+
+  useEffect(() => {
+    getMessages();
+  }, []);
+
+  return (
+    <div>
+      <h2>Messages</h2>
+      
+      <ul>
+        { messages && messages.map( (msg) => <li key={msg}>{msg}</li>)}
+      </ul>
+    </div>
+  );
+}
