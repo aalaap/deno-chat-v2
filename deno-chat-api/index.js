13028a36416d685a72d960ddd26e08a5c2a19252
Aalaap Ghag <aalaap@gmail.com>
2022-04-21 17:13:18 +0530

    Add broadcast channel

--- deno-chat-api/index.js
+++ deno-chat-api/index.js
@@ -2,7 +2,10 @@ import { Application, Router } from "https://deno.land/x/oak@v10.5.1/mod.ts";
 import { oakCors } from "https://deno.land/x/cors/mod.ts"
 
 const messages = [];
-
+const channel = new BroadcastChannel("chat");
+channel.onmessage = (e) => {
+  messages.push(event.data);
+}
 const router = new Router();
 router
   .get("/", (ctx) => ctx.response.body = "Chat server")
@@ -10,6 +13,8 @@ router
   .post("/messages", async (ctx) => {
     const message = await ctx.request.body().value;
     messages.push(message);
+    channel.postMessage(message);
+    ctx.response.body = messages;
   });
 
 const app = new Application();
